b5dbd4ba4fee60fc07cf6bc448216c4b7f5b16ee
RamTheToad <115056049+RamTheToad@users.noreply.github.com>
2024-11-16 12:36:29 -0800

    Update matrixAnalysis.py

--- matrixAnalysis.py
+++ matrixAnalysis.py
@@ -28,9 +28,7 @@ class MatrixAnalysis:
     
     @staticmethod
     def transpose(matrix1):
-        new_matrix=[]
-        for _ in range(len(matrix1[0])):
-            new_matrix.append([0] * len(matrix1))
+        new_matrix=[([0] * len(matrix1)) for _ in range(len(matrix1[0]))]
         for row in range(len(matrix1)):
             for col in range(len(matrix1[row])):
                 new_matrix[col][row] = matrix1[row][col]
@@ -38,19 +36,31 @@ class MatrixAnalysis:
 
     @staticmethod
     def flatten(matrix1):
-        flat_matrix = []
-        for row in range(len(matrix1)):
-            for col in range(len(matrix1[row])):
-               flat_matrix.append(matrix1[row][col])
+        flat_matrix = [matrix1[row][col] for row in range(len(matrix1)) for col in range(len(matrix1[row]))]
         return flat_matrix
 
+    #requires a square matrix
     @staticmethod
-    def determinant(matrix1, matrix2):
-        return "In development"
+    def determinant(matrix1):
+        print(matrix1)
+        determinant = 0
+        if len(matrix1)>2:
+            for item in range(len(matrix1[0])):
+                relevant_matrix = copy.deepcopy(matrix1)
+                relevant_matrix.pop(0)
+                print(matrix1)
+                for row in range(len(relevant_matrix)):
+                    relevant_matrix[row].pop(item)
+                    print(relevant_matrix)
+                determinant+=MatrixAnalysis.determinant(relevant_matrix)*matrix1[0][item]*pow(-1,item)
+        else:
+            print(matrix1)
+            determinant = matrix1[0][0]*matrix1[1][1]-matrix1[0][1]*matrix1[1][0]
+        return determinant
 
     #requires square matrix with non-zero determinant
     @staticmethod
-    def inverse(matrix1, matrix2):
+    def inverse(matrix1):
         return "In development"
     
     #only vectors
@@ -62,20 +72,13 @@ class MatrixAnalysis:
     #requires identical matrices
     @staticmethod
     def add(matrix1, matrix2):
-        result = [[matrix1[row][col] + matrix2[row][col] 
-                   for col in range(len(matrix1[row]))] 
-                   for row in range(len(matrix1))]
+        result = [[matrix1[row][col] + matrix2[row][col] for col in range(len(matrix1[row]))] for row in range(len(matrix1))]
         return result
     
     #requires identical matrices
     @staticmethod
     def subtract(matrix1, matrix2):
-        result = [[matrix1[row][col]-matrix2[row][col] 
-                   for col in range(len(matrix1[row]))] 
-                   for row in range(len(matrix1))]
-        # for row in range(len(matrix1)):
-        #     for col in range(len(matrix1[row])):
-        #         matrix1[row][col] -= matrix2[row][col]
+        result = [[matrix1[row][col] - matrix2[row][col] for col in range(len(matrix1[row]))] for row in range(len(matrix1))]
         return result
     
     #requires mxn and nxo matrices
@@ -100,9 +103,13 @@ matrixe = [
 vector=[1,2,3]
 vectore=[2,3,4]
 
+test = [[1,2,3],
+        [3,2,1],
+        [2,1,3]]
 #MatrixAnalysis.print_matrix(MatrixAnalysis.row_reduce(matrix))
-MatrixAnalysis.print_matrix(MatrixAnalysis.add(matrix,matrixe))
-MatrixAnalysis.print_matrix(MatrixAnalysis.subtract(matrix,matrixe))
-#MatrixAnalysis.print_matrix(MatrixAnalysis.flatten(matrix))
-#MatrixAnalysis.print_matrix(MatrixAnalysis.transpose(matrix))
+#MatrixAnalysis.print_matrix(MatrixAnalysis.add(matrix,matrixe))
+#MatrixAnalysis.print_matrix(MatrixAnalysis.subtract(matrix,matrixe))
+# print(MatrixAnalysis.flatten(matrix))
+# MatrixAnalysis.print_matrix(MatrixAnalysis.transpose(matrix))
 #print(MatrixAnalysis.dot(vector,vectore))
+print(MatrixAnalysis.determinant(test))
